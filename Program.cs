20aa72664b495b33f067b84280ac11952b498777
burakozturk05 <burak.ozturkk05@gmail.com>
2024-03-13 18:13:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+string[] kelimeler =new string[] { "Ahmet", "Mehmet", "Burak", "Sadık", "Remzi", "Ayşe" };
+Console.WriteLine(string.Join(" ",kelimeler));
+Random random = new Random();
+int tahminEdilen = random.Next(0, 6);
+
+Console.WriteLine("Lutfen tahmininizi girin.");
+bool gecerlimi = false;
+int hak = 3;
+while (hak > 0)
+{
+    string kelime = Console.ReadLine().Trim().ToLower();
+    bool bosmu = string.IsNullOrEmpty(kelime);
+    if (bosmu || int.TryParse(kelime, out int _))
+    {
+        Console.WriteLine("Lutfen Doğru bir karakter giriniz.");
+        hak--;
+    }
+    if (!(bosmu || int.TryParse(kelime, out int _)))
+    {
+        if (kelime == kelimeler[tahminEdilen].ToLower())
+        {
+            Console.WriteLine("Doğru;");
+            break;
+        }
+        if (!(kelime == kelimeler[tahminEdilen].ToLower()))
+        {
+            Console.WriteLine($"Yanlış.. Kelime '{kelimeler[tahminEdilen]}' olacaktı... YENİDEN DENEYİN");
+            hak--;
+        }
+    }
+}
+
+
+
